efe53cbd7eec6314ac5502aab44e79253dd036b2
andrew <neon.duncan@gmail.com>
2021-06-21 15:46:42 -0500

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,52 @@
  for you to use if you need it!
  */
 
+
+const createEmployeeRecord = (arr) => {
+    const employee = {
+        firstName:arr[0],
+        familyName:arr[1],
+        title:arr[2],
+        payPerHour:arr[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+    return employee
+}
+
+const createEmployeeRecords = (arrOfArrs) => {
+    return arrOfArrs.map(record => createEmployeeRecord(record))
+}
+
+const createTimeInEvent = (timeStamp) => {
+    const time = timeStamp.split(" ")
+    const event = {type:"TimeIn", hour: time[1], date: time[0]}
+    console.log(this)
+    //console.log(this.timeInEvents)
+    console.log(typeof this.timeInEvents)
+    //console.log(bpRecord)
+    //this.timeInEvents.push(event)
+}
+
+const createTimeOutEvent = (timeStamp) => {
+    const event = {type:"TimeOut", hour: time[1], date: time[0]} 
+    this.timeOutEvents.push()
+}
+const hoursWorkedOnDate = (date) => {
+
+}
+
+
+let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
+console.log(bpRecord)
+let updatedBpRecord = createTimeInEvent.call(bpRecord, "2014-02-28 1400")
+//let newEvent = updatedBpRecord.timeInEvents[0]
+
+
+
+
+
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
